8bacc96c8f33bc77e94e5be2ca79ecc04b692587
Colin de Vries <colindevries325@gmail.com>
2019-12-10 16:00:43 -0800

    still unable to return array with all values in individual arrays

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,17 +1,31 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
+def merge(arrA, arrB):
+    elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
-    # TO-DO
-    
+# 3. Start merging your single lists of one element together into larger, sorted sets
+# 4. Repeat step 3 until the entire data set has been reassembled
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
-    # TO-DO
+def merge_sort(arr):
+    # 1. While your data set contains more than one item, split it in half
+    # 2. Once you have gotten down to a single element, you have also * sorted * that element
+    # (a single element cannot be "out of order")
+    left_array = arr[:len(arr) // 2]
+    if len(arr) >= 2:
+        right_array = arr[len(arr) // 2: len(arr)]
 
+    print(left_array)
+    print(right_array)
+    while len(arr) > 1:
+        merge_sort(left_array)
+        merge_sort(right_array)
+        return arr
     return arr
+
+
+merge_sort([5, 2, 7, 12, 73, 21, 4, 18])
 
 
 # STRETCH: implement an in-place merge sort algorithm
@@ -20,7 +34,8 @@ def merge_in_place(arr, start, mid, end):
 
     return arr
 
-def merge_sort_in_place(arr, l, r): 
+
+def merge_sort_in_place(arr, l, r):
     # TO-DO
 
     return arr
@@ -28,6 +43,6 @@ def merge_sort_in_place(arr, l, r):
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+def timsort(arr):
 
     return arr
