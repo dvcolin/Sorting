0c6e874e4c8db19e26b00841bd81dbfdd4abea89
Colin de Vries <colindevries325@gmail.com>
2020-01-21 11:32:22 -0800

    completed merge sort in recursive_sorting assignment

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,22 +3,46 @@ def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
     # TO-DO
+    a_index = 0
+    b_index = 0
 
+    for i in range(elements):
+        # If there are no more elements in arrA, add element from arrB and b_index += 1
+        if a_index >= len(arrA):
+            merged_arr[i] = arrB[b_index]
+            b_index += 1
+        # If there are no more elements in arrB, add element from arrA and a_index += 1
+        elif b_index >= len(arrB):
+            merged_arr[i] = arrA[a_index]
+            a_index += 1
+        # If element in arrA is smaller, add element to merged array, a_index += 1
+        elif arrA[a_index] < arrB[b_index]:
+            merged_arr[i] = arrA[a_index]
+            a_index += 1
+        # If element in arrB is smaller, add element to merged array, b_index += 1
+        elif arrB[b_index] < arrA[a_index]:
+            merged_arr[i] = arrB[b_index]
+            b_index += 1
+        # If numbers are equal, add element from arrA and a_index += 1
+        else:
+            merged_arr[i] = arrA[a_index]
+            a_index += 1
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
     # TO-DO
-    arrA = arr[:len(arr) // 2]
-    arrB = arr[len(arr) // 2:]
-    print(arr)
+    mid_index = len(arr) // 2
+    arrA = arr[:mid_index]
+    arrB = arr[mid_index:]
     if len(arr) > 1:
-        return merge_sort(arrA), merge_sort(arrB)
-    return arr
+        arrA = merge_sort(arrA)
+        arrB = merge_sort(arrB)
 
+    sorted_arr = merge(arrA, arrB)
+    return sorted_arr
 
-print(merge_sort([6, 21, 7, 3, 26, 123, 73, 2]))
 
 # STRETCH: implement an in-place merge sort algorithm
 
